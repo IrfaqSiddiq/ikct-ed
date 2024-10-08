253e6656a838cb797eba97f3b777dd0707e6f5f5
Irfaq <mohammed.irfaq786@gmail.com>
2024-10-08 16:11:30 +0530

    ADDED domain instead of HOST

--- services/services.go
+++ services/services.go
@@ -8,6 +8,6 @@ import (
 
 // RemoveCookies used to remove cookies
 func RemoveCookies(c *gin.Context, cookiesName string) {
-	domain := os.Getenv("HOST")
+	domain := os.Getenv("DOMAIN")
 	c.SetCookie(cookiesName, "", -1, "/", domain, false, true)
 }
